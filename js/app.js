dd57a840ead8860513a83e82631c68e669688e4f
MaionePietro <pietromaionespam@gmail.com>
2023-02-15 15:13:56 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//arrey immagini
+const arrayImg = ['./img/1.jpg','./img/2.jpg','./img/3.jpg'];
+
+
+let indiceSlideAttiva = 0
+const slideElements = document.getElementsByClassName('slide')
+console.log(slideElements)
+
+const rightBtnElement = document.querySelector('.carosello__arrow.arrow-right')
+const leftBtnElement = document.querySelector('.carosello__arrow.arrow-left')
+
+console.log(rightBtnElement)
+
+rightBtnElement.addEventListener('click', function () {
+	console.log('current slide', indiceSlideAttiva)
+    
+	let slideCorrente = slideElements[indiceSlideAttiva]
+	slideCorrente.classList.remove('active')
+
+	indiceSlideAttiva += 1  
+
+	let prossimaSlide = slideElements[indiceSlideAttiva]
+	prossimaSlide.classList.add('active')
+
+	console.log('next slide', indiceSlideAttiva)
+})
+
+leftBtnElement.addEventListener('click', function () {
+	console.log('current slide', indiceSlideAttiva)
+
+    let slideCorrente = slideElements[indiceSlideAttiva]
+	slideCorrente.classList.remove('active')
+
+	indiceSlideAttiva -= 1
+
+	let prossimaSlide = slideElements[indiceSlideAttiva]
+	prossimaSlide.classList.add('active')
+
+	console.log('next slide', indiceSlideAttiva)
+})
