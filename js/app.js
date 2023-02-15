e8f647a3245b6a26965cfdf853b615656b21443c
MaionePietro <pietromaionespam@gmail.com>
2023-02-15 15:33:00 +0100

    FEAT object img

--- js/app.js
+++ js/app.js
@@ -1,40 +1,28 @@
-//arrey immagini
 
-
-
-let indiceSlideAttiva = 0
-const slideElements = document.getElementsByClassName('slide')
-console.log(slideElements)
-
-const rightBtnElement = document.querySelector('.carosello__arrow.arrow-right')
-const leftBtnElement = document.querySelector('.carosello__arrow.arrow-left')
-
-console.log(rightBtnElement)
-
-rightBtnElement.addEventListener('click', function () {
-	console.log('current slide', indiceSlideAttiva)
-    
-	let slideCorrente = slideElements[indiceSlideAttiva]
-	slideCorrente.classList.remove('active')
-
-	indiceSlideAttiva += 1  
-
-	let prossimaSlide = slideElements[indiceSlideAttiva]
-	prossimaSlide.classList.add('active')
-
-	console.log('next slide', indiceSlideAttiva)
-})
-
-leftBtnElement.addEventListener('click', function () {
-	console.log('current slide', indiceSlideAttiva)
-
-    let slideCorrente = slideElements[indiceSlideAttiva]
-	slideCorrente.classList.remove('active')
-
-	indiceSlideAttiva -= 1
-
-	let prossimaSlide = slideElements[indiceSlideAttiva]
-	prossimaSlide.classList.add('active')
-
-	console.log('next slide', indiceSlideAttiva)
-})
+const img =[
+	{
+		ulr :'01.webp',
+		titolo :'Spider-Man: Miles Morales',
+		descrizione: 'Marvels Spider-Man: Miles Morales è un videogioco d avventura dinamica sviluppato da Insomniac Games e distribuito da Sony Interactive Entertainment in esclusiva per PlayStation 4 e PlayStation 5.'
+	},
+	{
+		url :'02.webp',
+		titolo :'Ratchet & Clank: Rift Apart',
+		descrizione :'Ratchet & Clank: Rift Apart è un videogioco a piattaforme e sparatutto in terza persona sviluppato da Insomniac Games e pubblicato da Sony Interactive Entertainment per PlayStation 5. Il titolo segna il debutto della serie Ratchet & Clank su PS5 e racconta gli eventi successivi a Ratchet & Clank: Nexus.',
+	},
+	{
+		url :'03.webp',
+		titolo :'Fortnite',
+		descrizione :'Fortnite è un videogioco sparatutto in terza persona del 2017, sviluppato da People Can Fly e pubblicato da Epic Games per console e PC. Il gioco presenta tre modalità distinte che condividono lo stesso motore grafico: Salva il mondo, Modalità creativa e Battaglia reale.',
+	},
+	{
+		url :'04.webp',
+		titolo :'Stray',
+		descrizione :'Stray è un videogioco di avventura dinamica del 2022 sviluppato da BlueTwelve Studio e pubblicato dall Annapurna Interactive per PlayStation 4, PlayStation 5 e Microsoft Windows.',
+	},
+	{
+		url :'05.webp',
+		titolo :'Marvels Avengers',
+		descrizione :'Marvels Avengers è un videogioco action-adventure sviluppato dalla Crystal Dynamics e Eidos Montréal, e pubblicato dalla Square Enix. È il primo nuovo titolo della Crystal Dynamics al di fuori del franchise di Tomb Raider partito nel 2005, e si basa sul gruppo di supereroi della Marvel Comics, i Vendicatori.',
+	}
+]
